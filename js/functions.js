451be0f7bc60c127c4190dc77fbd9f13c46d1d1d
Frey C. T <48113776+PanderIT@users.noreply.github.com>
2019-04-11 22:57:36 -0700

    Unfinished Portfolio
    I will finish this over time.

---
+++ js/functions.js
@@ -0,0 +1,47 @@
+var count = 0;
+
+function howmany(){
+    count += 1;
+    if (count % 2 === 1){
+        document.getElementById('xlanguages').innerHTML = 'I know Six languages if you count html and css.';
+    }
+    else{
+        document.getElementById('xlanguages').innerHTML = 'Click Me to Find Out.'
+    }
+}
+
+var db = firebase.firestore();
+
+function send() {
+    alert("adding data");
+    var first_name = document.getElementById("firstName").value;
+    var last_name = document.getElementById("lastName").value;
+    var query = document.getElementById("comments").value;
+    var e_mail = document.getElementById("emails").value;
+    db.collection("contact").add({
+        comment: query,
+        email: e_mail,
+        fName: first_name,
+        lName: last_name
+    }).then(function (docRef) {
+        console.log("Document written with ID: ", docRef.id);
+        //update the products view
+        getProducts();
+    }).catch(function (error) {
+        console.error("Error adding document: ", error);
+    });
+    alert('Information Sent to Freys Database.')
+}
+
+
+var productdisplay = document.getElementById('products');
+productdisplay.innerHTML = "";var db = firebase.firestore();
+db.collection("game").get().then((querySnapshot) =>{
+
+    alert("Data Recieved");
+    productdisplay.innerHTML = "";
+    querySnapshot.forEach((doc) => {
+        productdisplay.innerHTML += (doc.data().productName + " " + doc.data().productPrice + " "
+            + "<img src='images/" + doc.data().productImage + "'>" +"<br>");
+    });
+});

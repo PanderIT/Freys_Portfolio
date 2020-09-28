e7abfe941f8d3bdb66c26abdef727ecd7d6b457e
PanderIT <FreydenIT@gmail.com>
2020-09-28 16:26:34 -0700

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -39,15 +39,11 @@ function lab1Solution() {
 }
 
 function lab2Solution() {
-    try {
-        let x = parseInt(document.getElementById("numberInput").value);
-        if (x == NaN) {
-            throw TypeError
-        }
-      }
-      catch(err) {
+
+    let x = parseInt(document.getElementById("numberInput").value);
+    if (x === NaN) {
         alert("Invalid input, please enter a number from 1 - 26!~")
-      }
+    }
     
     console.log(x);
 }

82a350145b5d7696a6f5d7f23373b50f3c4179f6
PanderIT <FreydenIT@gmail.com>
2020-10-10 03:27:43 -0700

    Lab3
    added Get Time.

--- js/functions.js
+++ js/functions.js
@@ -106,41 +106,17 @@ class Recipe {
             recipes.appendChild(element)
         }
     }
-  }
+}
 
-
-// var db = firebase.firestore();
-
-// function send() {
-//     alert("adding data");
-//     var first_name = document.getElementById("firstName").value;
-//     var last_name = document.getElementById("lastName").value;
-//     var query = document.getElementById("comments").value;
-//     var e_mail = document.getElementById("emails").value;
-//     db.collection("contact").add({
-//         comment: query,
-//         email: e_mail,
-//         fName: first_name,
-//         lName: last_name
-//     }).then(function (docRef) {
-//         console.log("Document written with ID: ", docRef.id);
-//         //update the products view
-//         getProducts();
-//     }).catch(function (error) {
-//         console.error("Error adding document: ", error);
-//     });
-//     alert('Information Sent to Freys Database.')
-// }
-
-
-// var productdisplay = document.getElementById('products');
-// productdisplay.innerHTML = "";var db = firebase.firestore();
-// db.collection("game").get().then((querySnapshot) =>{
-
-//     alert("Data Recieved");
-//     productdisplay.innerHTML = "";
-//     querySnapshot.forEach((doc) => {
-//         productdisplay.innerHTML += (doc.data().productName + " " + doc.data().productPrice + " "
-//             + "<img src='images/" + doc.data().productImage + "'>" +"<br>");
-//     });
-// });
+function getTime() {
+    let xhttp = new XMLHttpRequest();
+    let str = document.getElementById("numberInput").value;
+    xhttp.open("GET", "http://localhost:8888/?name=" + str, true);
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML =
+                this.responseText;
+        }
+    };
+}

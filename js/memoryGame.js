98af0cebc5b7ad0abed44a487516f8f6e353d384
PanderIT <FreydenIT@gmail.com>
2020-10-29 14:50:08 -0700

    Update memoryGame.js

--- js/memoryGame.js
+++ js/memoryGame.js
@@ -123,7 +123,7 @@ function updateHighscore() {
     let score = parseInt(document.getElementById("score").innerHTML);
     let highscore = parseInt(document.getElementById("score").innerHTML);
     document.getElementById("highscore").innerHTML = (score > highscore) ? score : highscore;
-    if(document.getElementById("name").innerHTML === "") {
+    if(document.getElementById("name").value === "") {
         alert("Warning, Please enter your name if you want your score to be recorded.");
         return;
     }

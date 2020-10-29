aa166a37fb588803968c8fa0b79e268dc4cc7010
PanderIT <FreydenIT@gmail.com>
2020-10-29 15:02:11 -0700

    Update memoryGame.js

--- js/memoryGame.js
+++ js/memoryGame.js
@@ -122,7 +122,7 @@ function cellClicked() {
 function updateHighscore() {
     let score = parseInt(document.getElementById("score").innerHTML);
     let highscore = parseInt(document.getElementById("score").innerHTML);
-    let name = document.getElementById("name").innerText;
+    let name = document.getElementById("name").innerHTML;
     document.getElementById("highscore").innerHTML = (score > highscore) ? score : highscore;
     console.log(name);
     if(name === "") {

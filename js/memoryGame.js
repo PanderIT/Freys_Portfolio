3c6e09fdddc29002cf313abd0f1cf742e049645e
PanderIT <FreydenIT@gmail.com>
2020-10-28 17:26:31 -0700

    Update memoryGame.js

--- js/memoryGame.js
+++ js/memoryGame.js
@@ -55,7 +55,7 @@ function generateGame() {
         let cell = document.createElement("div");
         cell.setAttribute("class", "cell");
         cell.setAttribute("id", `${"cell" + (i + 1)}`);
-        cell.setAttribute("style", "background: white");
+        cell.setAttribute("style", "background: purple");
         grid.appendChild(cell);
     }
     console.log(`memoryCells: ${memoryCells}`);

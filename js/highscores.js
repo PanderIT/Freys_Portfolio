7d01176e07fa5f64b0ddb38ca6001345578bb2f4
PanderIT <FreydenIT@gmail.com>
2020-10-29 19:27:26 -0700

    A1

--- js/highscores.js
+++ js/highscores.js
@@ -1,4 +1,4 @@
-function getHighscores() {
+function requestHighscores() {
     console.log("HTTTTTTTTP");
 
     let xhttp = new XMLHttpRequest();
@@ -7,12 +7,37 @@ function getHighscores() {
         if (this.readyState == 4 && this.status == 200) {
             arr = JSON.parse(this.responseText);
             console.log(JSON.parse(this.responseText));
-
+            let i = 1;
             arr.forEach(row => {
-                console.log(row["name"] + " " + row["score"]);
+                showHighscores(i, row["name"], row["score"]);
+                i += 1;
             });
         }
     };
     xhttp.open("GET", `https://memorygamedbserver.herokuapp.com/`, true);
     xhttp.send();
+}
+
+function showHighscores(place, name, score) {
+    let highscores = document.getElementById("highscores");
+    let row = document.createElement("div");
+    let p = document.createElement("div"); 
+    let n = document.createElement("div"); 
+    let s = document.createElement("div");
+    
+    p.setAttribute("class", "col-4");
+    p.innerHTML = place;
+
+    n.setAttribute("class", "col-4");
+    n.innerHTML = name;
+
+    s.setAttribute("class", "col-4");
+    s.innerHTML = score;
+
+    row.setAttribute("class", "row");
+    row.appendChild(p);
+    row.appendChild(n);
+    row.appendChild(s);
+
+    highscores.appendChild(row);
 }

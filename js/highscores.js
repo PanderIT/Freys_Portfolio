ff708e91c9d32f784171880588aeed6b91c971ed
PanderIT <FreydenIT@gmail.com>
2020-10-29 19:11:04 -0700

    Update highscores.js

--- js/highscores.js
+++ js/highscores.js
@@ -2,13 +2,17 @@ function getHighscores() {
     console.log("HTTTTTTTTP");
 
     let xhttp = new XMLHttpRequest();
-
+    let arr = [];
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
+            arr = JSON.parse(this.responseText);
             console.log(JSON.parse(this.responseText));
         }
     };
     xhttp.open("GET", `https://memorygamedbserver.herokuapp.com/`, true);
     xhttp.send();
     
+    arr.forEach(row => {
+        console.log(row["name"] + " " + row["score"]);
+    });
 }

6c963539908f2bcd55dead488454bee32c753ede
PanderIT <FreydenIT@gmail.com>
2020-11-12 09:36:23 -0800

    sfsdf

---
+++ js/lab5.js
@@ -0,0 +1,33 @@
+function saveDefinition() {
+    let xhttp = new XMLHttpRequest();
+    let word = document.getElementById("word").value;
+    let definition = document.getElementById("definition").value;
+    console.log(word + ": " + definition);
+    xhttp.open("POST", `https://memorygamedbserver.herokuapp.com/addDefinition/${word}/${definition}`, true);
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    };
+}
+
+function getDefinition() {
+
+    let xhttp = new XMLHttpRequest();
+    let arr = [];
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            arr = JSON.parse(this.responseText);
+            console.log(JSON.parse(this.responseText));
+            let i = 1;
+            showHighscores("Rank", "Name", "Score");
+            arr.forEach(row => {
+                showHighscores(i, row["name"], row["score"]);
+                i += 1;
+            });
+        }
+    };
+    xhttp.open("GET", `https://memorygamedbserver.herokuapp.com/api/definitions`, true);
+    xhttp.send();
+}

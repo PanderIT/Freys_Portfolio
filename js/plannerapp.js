9e78ff8168e42ec4b5336b6cc484949248affb3b
PanderIT <FreydenIT@gmail.com>
2020-12-02 01:14:19 -0800

    Update plannerapp.js

--- js/plannerapp.js
+++ js/plannerapp.js
@@ -1,9 +1,19 @@
 const search = document.getElementById("search");
-search.addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') {
-      search();
-    }
-})
+const t06 = document.getElementById("t06");
+const t07 = document.getElementById("t07");
+const t08 = document.getElementById("t08");
+const t09 = document.getElementById("t09");
+const t10 = document.getElementById("t10");
+const t11 = document.getElementById("t11");
+const t12 = document.getElementById("t12");
+const t13 = document.getElementById("t13");
+const t14 = document.getElementById("t14");
+const t15 = document.getElementById("t15");
+const t16 = document.getElementById("t16");
+const t17 = document.getElementById("t17");
+const t18 = document.getElementById("t18");
+const t19 = document.getElementById("t19");
+const t20 = document.getElementById("t20");
 
 function save() {
     console.log("hello World");
@@ -22,3 +32,99 @@ function next() {
 function search() {
     save();
 }
+
+search.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      search();
+    }
+})
+
+t06.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      search();
+    }
+})
+
+t07.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      search();
+    }
+})
+
+t08.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      search();
+    }
+})
+
+t09.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      search();
+    }
+})
+
+t10.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      search();
+    }
+})
+
+t11.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      search();
+    }
+})
+
+t12.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      search();
+    }
+})
+
+t13.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      search();
+    }
+})
+
+t14.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      search();
+    }
+})
+
+t15.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      search();
+    }
+})
+
+t16.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      search();
+    }
+})
+
+t17.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      search();
+    }
+})
+
+t18.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      search();
+    }
+})
+
+t19.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      search();
+    }
+})
+
+t20.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      search();
+    }
+})

b4e59d9bc7070e6e8bd5438b94022b4f247b60d2
PanderIT <FreydenIT@gmail.com>
2020-12-02 01:49:05 -0800

    fddf

--- js/plannerapp.js
+++ js/plannerapp.js
@@ -1,26 +1,3 @@
-let srch = document.getElementById("search");
-srch.addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') {
-      search();
-    }
-})
-
-let t06 = document.getElementById("t06");
-let t07 = document.getElementById("t07");
-let t08 = document.getElementById("t08");
-let t09 = document.getElementById("t09");
-let t10 = document.getElementById("t10");
-let t11 = document.getElementById("t11");
-let t12 = document.getElementById("t12");
-let t13 = document.getElementById("t13");
-let t14 = document.getElementById("t14");
-let t15 = document.getElementById("t15");
-let t16 = document.getElementById("t16");
-let t17 = document.getElementById("t17");
-let t18 = document.getElementById("t18");
-let t19 = document.getElementById("t19");
-let t20 = document.getElementById("t20");
-
 function today() {
 
 }
@@ -43,94 +20,9 @@ function search() {
     save();
 }
 
-
-
-t06.addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') {
-      search();
-    }
-})
-
-t07.addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') {
-      search();
-    }
-})
-
-t08.addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') {
-      search();
-    }
-})
-
-t09.addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') {
-      search();
-    }
-})
-
-t10.addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') {
-      search();
-    }
-})
-
-t11.addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') {
-      search();
-    }
-})
-
-t12.addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') {
-      search();
-    }
-})
-
-t13.addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') {
-      search();
-    }
-})
-
-t14.addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') {
-      search();
-    }
-})
-
-t15.addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') {
-      search();
-    }
-})
-
-t16.addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') {
-      search();
-    }
-})
-
-t17.addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') {
-      search();
-    }
-})
-
-t18.addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') {
-      search();
-    }
-})
-
-t19.addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') {
-      search();
-    }
-})
-
-t20.addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') {
-      search();
-    }
-})
+$('#search').datepicker({
+    format: "dd/mm/yy",
+    calendarWeeks: true,
+    autoclose: true,
+    todayHighlight: true
+});
